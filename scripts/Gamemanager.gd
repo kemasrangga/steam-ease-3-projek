869438639606e8dccb246e31c175fddeaa97151a
Powpowhero1210 <akhtars2008@gmail.com>
2026-05-15 12:54:00 +0700

    adding title_screen

--- scripts/Gamemanager.gd
+++ scripts/Gamemanager.gd
@@ -1,13 +1,27 @@
 extends Node
 
 var current_level = 1
+var final_level = 3
 var area_path = "res://Levels/"
 
+func end_game():
+	print("the game has finished")
+	var full_path = area_path + "Title_screen.tscn"
+	get_tree().change_scene_to_file(full_path)
+	
 func next_level():
+	
+	#checks for title screen
+	if current_level == final_level:
+		end_game()
+		return
+	
 	current_level += 1
+	
 	var full_path = area_path + "level_" + str(current_level) + ".tscn"
 	if !full_path:
 		print("error: level file not found!")
 		return
 	get_tree().change_scene_to_file(full_path)
+	
 	print("Player is sent to next level")
